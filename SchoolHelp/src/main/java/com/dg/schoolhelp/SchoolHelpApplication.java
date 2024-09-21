962eef99b1f60d11624c642bb172b560004f03cd
DG <xiaohaodong4@gmail.com>
2024-09-22 01:12:44 +0800

    SpringBoot init

---
+++ SchoolHelp/src/main/java/com/dg/schoolhelp/SchoolHelpApplication.java
@@ -0,0 +1,11 @@
+package com.dg.schoolhelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolHelpApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolHelpApplication.class, args);
+    }
+}
